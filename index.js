e8502d5c36c034cc91fc3c67877f298c65630f84
Michiel Sikma <michiel@sikma.org>
2021-09-13 15:14:49 +0200

    A number of changes - things work more consistently, isExecutable/isTerminator/isAfterTerminator are null when they do not apply, plus a bunch of fixes

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 // cmd-tokenize <https://github.com/msikma/cmd-tokenize>
 // © MIT license
 
-const { argumentMetadataStandalone } = require('./lib/metadata/arguments')
-const { escapeArgument } = require('./lib/escape')
-const { addSlashes, removeSlashes } = require('./lib/escape/slashes')
 const { parseCommand, splitCommand, parseArguments, splitArguments } = require('./lib')
+const { addSlashes, removeSlashes } = require('./lib/escape/slashes')
+const { escapeArgument } = require('./lib/escape')
+const { parseArgument } = require('./lib/metadata/arguments')
+const { splitArgumentBlocks } = require('./lib/split')
+const { EscapeError, ParseError } = require('./lib/error')
 
 module.exports = {
   parseCommand,
   splitCommand,
   parseArguments,
   splitArguments,
+  exception: {
+    EscapeError,
+    ParseError
+  },
   util: {
-    argumentMetadata: argumentMetadataStandalone,
+    splitArgumentBlocks,
+    parseArgument,
     escapeArgument,
     addSlashes,
     removeSlashes
